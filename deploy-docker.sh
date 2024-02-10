441bf158d99c7749c1f0cbaab691dabcdd2d9ebf
coder <usmccoder@gmail.com>
2024-02-10 09:49:00 -0700

    docker updates

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -3,6 +3,6 @@
 REGISTRY="sgtcoder"
 PROJECT_NAME="apache-base"
 
-#./deploy.sh --no-cache
+#./deploy-docker.sh --no-cache
 docker build $1 -t $REGISTRY/$PROJECT_NAME . || exit 1
 docker push $REGISTRY/$PROJECT_NAME
